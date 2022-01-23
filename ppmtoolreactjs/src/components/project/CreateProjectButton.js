eb610a9263766a0c79ea63f9eaf16524bb2de4dc
harshpandyahcl <harsh.pandya@hcl.com>
2022-01-22 19:16:43 -0600

    branch19 connected with backend

--- ppmtoolreactjs/src/components/project/CreateProjectButton.js
+++ ppmtoolreactjs/src/components/project/CreateProjectButton.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+
+import React from "react";
+import { Link } from "react-router-dom";
 
 const CreateProjectButton = () => {
-    return (
-      <React.Fragment>
-        <Link to="/addProject" className="btn btn-lg btn-info">
-          Create a Project
-        </Link>
-      </React.Fragment>
-    );
-  };
-  
-  export default CreateProjectButton;
+  return (
+    <React.Fragment>
+      <Link to="/addProject" className="btn btn-lg btn-info">
+        Create a Project
+      </Link>
+    </React.Fragment>
+  );
+};
+
+export default CreateProjectButton;
