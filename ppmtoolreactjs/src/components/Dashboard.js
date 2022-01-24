9345d52185b583781f80ee3441c3cbda46a8ed6d
harshpandyahcl <harsh.pandya@hcl.com>
2022-01-24 17:38:41 -0600

    updated dashboard and additeam,js

--- ppmtoolreactjs/src/components/Dashboard.js
+++ ppmtoolreactjs/src/components/Dashboard.js
@@ -1,21 +1,32 @@
 import React, { Component } from "react";
 import ProjectItem from "./project/ProjectItem";
 import CreateProjectButton from "./project/CreateProjectButton";
-
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
+import PropTypes from "prop-types";
 class Dashboard extends Component {
+  componentDidMount() {
+    this.props.getProjects();
+  }
+
   render() {
+    const { projects } = this.props.project;
+
     return (
       <div className="projects">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <h1 className="display-4 text-center">PROJECTS</h1>
+              <h1 className="display-4 text-center">Projects</h1>
               <br />
               <CreateProjectButton />
 
               <br />
               <hr />
-              <ProjectItem />
+              
+              {projects.map(project => (
+                <ProjectItem key={project.id} project={project} />
+              ))}
             </div>
           </div>
         </div>
@@ -23,5 +34,14 @@ class Dashboard extends Component {
     );
   }
 }
-
-export default Dashboard;
+Dashboard.propTypes = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired
+};
+const mapStateToProps = state => ({
+  project: state.project
+});
+export default connect(
+  mapStateToProps,
+  { getProjects }
+)(Dashboard);
