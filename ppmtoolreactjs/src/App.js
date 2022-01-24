fcd9532a9247af8e13bf62b0a967f2cc9bb1113b
harshpandyahcl <harsh.pandya@hcl.com>
2022-01-24 17:57:02 -0600

    added update js

--- ppmtoolreactjs/src/App.js
+++ ppmtoolreactjs/src/App.js
@@ -7,7 +7,7 @@ import Header from './components/layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Provider} from "react-redux";
 import store from './store';
-
+import UpdateProject from "./components/project/UpdateProject";
 
 
 class App extends Component {
@@ -19,6 +19,7 @@ class App extends Component {
             <Header />
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
+            <Route exact path="/updateProject/:id" component={UpdateProject} />
           </div>
         </Router>
       </Provider>
