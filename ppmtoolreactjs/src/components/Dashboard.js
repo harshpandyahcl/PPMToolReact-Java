539209afb65d7e2aab08033578b68edb77c27dbb
harshpandyahcl <harsh.pandya@hcl.com>
2022-01-24 17:07:05 -0600

    branch 22 updated dashboard

--- ppmtoolreactjs/src/components/Dashboard.js
+++ ppmtoolreactjs/src/components/Dashboard.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 import ProjectItem from "./project/ProjectItem";
 import CreateProjectButton from "./project/CreateProjectButton";
-
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
+import PropTypes from "prop-types";
 class Dashboard extends Component {
+  componentDidMount() {
+    this.props.getProjects();
+  }
   render() {
     return (
       <div className="projects">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <h1 className="display-4 text-center">PROJECTS</h1>
+              <h1 className="display-4 text-center">Projects</h1>
               <br />
               <CreateProjectButton />
-
               <br />
               <hr />
               <ProjectItem />
@@ -23,5 +27,19 @@ class Dashboard extends Component {
     );
   }
 }
+Dashboard.propTypes = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired
+};
 
-export default Dashboard;
+// const mapStateToProps = state => ({
+//   project: state.project
+// });
+const mapStateToProps = state => ({
+  project: state.project
+});
+
+export default connect(
+  mapStateToProps,
+  { getProjects }
+)(Dashboard);
