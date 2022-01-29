bbe29a026520a16870d2eefac6455c81dff4c315
harshpandyahcl <harsh.pandya@hcl.com>
2022-01-29 16:03:31 -0600

    post projec task

--- ppmtoolreactjs/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
+++ ppmtoolreactjs/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
@@ -4,12 +4,10 @@ import { connect } from "react-redux";
 import classnames from "classnames";
 import { addProjectTask } from "../../../actions/backlogActions";
 import PropTypes from "prop-types";
-
 class AddProjectTask extends Component {
   constructor(props) {
     super(props);
     const { id } = this.props.match.params;
-
     this.state = {
       summary: "",
       acceptanceCriteria: "",
@@ -19,11 +17,34 @@ class AddProjectTask extends Component {
       projectIdentifier: id,
       errors: {}
     };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  // on change
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  //on submit
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newTask = {
+      summary: this.state.summary,
+      acceptanceCriteria: this.state.acceptanceCriteria,
+      status: this.state.status,
+      priority: this.state.priority,
+      dueDate: this.state.dueDate
+    };
+    this.props.addProjectTask(
+      this.state.projectIdentifier,
+      newTask,
+      this.props.history
+    );
   }
 
   render() {
     const { id } = this.props.match.params;
-
     return (
       <div className="add-PBI">
         <div className="container">
@@ -34,7 +55,8 @@ class AddProjectTask extends Component {
               </Link>
               <h4 className="display-4 text-center">Add Project Task</h4>
               <p className="lead text-center">Project Name + Project Code</p>
-              <form>
+              
+              <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
                     type="text"
@@ -42,6 +64,7 @@ class AddProjectTask extends Component {
                     name="summary"
                     placeholder="Project Task summary"
                     value={this.state.summary}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
@@ -50,6 +73,7 @@ class AddProjectTask extends Component {
                     placeholder="Acceptance Criteria"
                     name="acceptanceCriteria"
                     value={this.state.acceptanceCriteria}
+                    onChange={this.onChange}
                   />
                 </div>
                 <h6>Due Date</h6>
@@ -59,6 +83,7 @@ class AddProjectTask extends Component {
                     className="form-control form-control-lg"
                     name="dueDate"
                     value={this.state.dueDate}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
@@ -66,6 +91,7 @@ class AddProjectTask extends Component {
                     className="form-control form-control-lg"
                     name="priority"
                     value={this.state.priority}
+                    onChange={this.onChange}
                   >
                     <option value={0}>Select Priority</option>
                     <option value={1}>High</option>
@@ -78,6 +104,7 @@ class AddProjectTask extends Component {
                     className="form-control form-control-lg"
                     name="status"
                     value={this.state.status}
+                    onChange={this.onChange}
                   >
                     <option value="">Select Status</option>
                     <option value="TO_DO">TO DO</option>
@@ -97,12 +124,9 @@ class AddProjectTask extends Component {
     );
   }
 }
-
-
 AddProjectTask.propTypes = {
   addProjectTask: PropTypes.func.isRequired
 };
-
 export default connect(
   null,
   { addProjectTask }
