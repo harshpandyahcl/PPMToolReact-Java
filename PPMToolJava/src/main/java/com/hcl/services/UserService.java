56c6a0856d36b9233f7f8e5b42dc9cca1286a103
harshpandyahcl <harsh.pandya@hcl.com>
2022-01-31 21:30:27 -0600

    user object, repo, validation

---
+++ PPMToolJava/src/main/java/com/hcl/services/UserService.java
@@ -0,0 +1,15 @@
+package com.hcl.services;
+
+import com.hcl.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+
+}
