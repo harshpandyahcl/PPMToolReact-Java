8ec688a32eb7f9f9cfadf679702fb70998617fca
harshpandyahcl <harsh.pandya@hcl.com>
2022-02-01 11:56:14 -0600

    user validation

--- PPMToolJava/src/main/java/com/hcl/web/UserController.java
+++ PPMToolJava/src/main/java/com/hcl/web/UserController.java
@@ -1,9 +1,10 @@
 package com.hcl.web;
 
+
 import com.hcl.domain.User;
 import com.hcl.services.MapValidationErrorService;
 import com.hcl.services.UserService;
-
+import com.hcl.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,28 +13,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.validation.Valid;
-
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-
     @Autowired
     private MapValidationErrorService mapValidationErrorService;
-
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private UserValidator userValidator;
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
         // Validate passwords match
+        userValidator.validate(user,result);
 
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
         if(errorMap != null)return errorMap;
-
         User newUser = userService.saveUser(user);
-
         return  new ResponseEntity<User>(newUser, HttpStatus.CREATED);
     }
 }
