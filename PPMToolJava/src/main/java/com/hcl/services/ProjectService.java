2c0f8a36f5ebe30daf85d2b2b552be9c0db14212
harshpandyahcl <harsh.pandya@hcl.com>
2022-02-02 15:45:58 -0600

    Lock operations to specific users

--- PPMToolJava/src/main/java/com/hcl/services/ProjectService.java
+++ PPMToolJava/src/main/java/com/hcl/services/ProjectService.java
@@ -20,8 +20,20 @@ public class ProjectService {
     private BacklogRepository backlogRepository;
     @Autowired
     private UserRepository userRepository;
+
     public Project saveOrUpdateProject(Project project, String username){
+
+        if(project.getId() != null){
+            Project existingProject = projectRepository.findByProjectIdentifier(project.getProjectIdentifier());
+            if(existingProject !=null &&(!existingProject.getProjectLeader().equals(username))){
+                throw new ProjectNotFoundException("Project not found in your account");
+            }else if(existingProject == null){
+                throw new ProjectNotFoundException("Project with ID: '"+project.getProjectIdentifier()+"' cannot be updated because it doesn't exist");
+            }
+        }
+
         try{
+
             User user = userRepository.findByUsername(username);
             project.setUser(user);
             project.setProjectLeader(user.getUsername());
@@ -40,40 +52,21 @@ public class ProjectService {
             throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
         }
     }
-
-
-
-        public Project findProjectByIdentifier(String projectId, String username){
-
-            //Only want to return the project if the user looking for it is the owner
-
-            Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
-            if(project == null){
-                throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
-
-            }
-
-            if(!project.getProjectLeader().equals(username)){
-                throw new ProjectNotFoundException("Project not found in your account");
-            }
-
-
-
-            return project;
+    public Project findProjectByIdentifier(String projectId, String username){
+        //Only want to return the project if the user looking for it is the owner
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project == null){
+            throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
         }
-
-
-            public Iterable<Project> findAllProjects(String username){
-                return projectRepository.findAllByProjectLeader(username);
-            }
-
-
-                public void deleteProjectByIdentifier(String projectid, String username){
-
-
-
-
-                    projectRepository.delete(findProjectByIdentifier(projectid, username));
-                }
-
-            }
+        if(!project.getProjectLeader().equals(username)){
+            throw new ProjectNotFoundException("Project not found in your account");
+        }
+        return project;
+    }
+    public Iterable<Project> findAllProjects(String username){
+        return projectRepository.findAllByProjectLeader(username);
+    }
+    public void deleteProjectByIdentifier(String projectid, String username){
+        projectRepository.delete(findProjectByIdentifier(projectid, username));
+    }
+}
