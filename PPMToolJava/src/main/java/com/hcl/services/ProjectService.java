63dba65af1e204304fc9183caaf21a52dee9f9f1
harshpandyahcl <harsh.pandya@hcl.com>
2022-02-02 15:27:30 -0600

    Lock operations to specific users

--- PPMToolJava/src/main/java/com/hcl/services/ProjectService.java
+++ PPMToolJava/src/main/java/com/hcl/services/ProjectService.java
@@ -5,6 +5,7 @@ import com.hcl.domain.Backlog;
 import com.hcl.domain.Project;
 import com.hcl.domain.User;
 import com.hcl.exceptions.ProjectIdException;
+import com.hcl.exceptions.ProjectNotFoundException;
 import com.hcl.repositories.BacklogRepository;
 import com.hcl.repositories.ProjectRepository;
 import com.hcl.repositories.UserRepository;
@@ -17,47 +18,62 @@ public class ProjectService {
     private ProjectRepository projectRepository;
     @Autowired
     private BacklogRepository backlogRepository;
-
-        @Autowired
-        private UserRepository userRepository;
-
-        public Project saveOrUpdateProject(Project project, String username){
-            try{
-                User user = userRepository.findByUsername(username);
-                project.setUser(user);
-                project.setProjectLeader(user.getUsername());
-                project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
-
-                if(project.getId()==null){
-                    Backlog backlog = new Backlog();
-                    project.setBacklog(backlog);
-                    backlog.setProject(project);
-                    backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
-                }
-                if(project.getId()!=null){
-                    project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
-                }
-                return projectRepository.save(project);
-            }catch (Exception e){
-                throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+    @Autowired
+    private UserRepository userRepository;
+    public Project saveOrUpdateProject(Project project, String username){
+        try{
+            User user = userRepository.findByUsername(username);
+            project.setUser(user);
+            project.setProjectLeader(user.getUsername());
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            if(project.getId()==null){
+                Backlog backlog = new Backlog();
+                project.setBacklog(backlog);
+                backlog.setProject(project);
+                backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
             }
+            if(project.getId()!=null){
+                project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
+            }
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
         }
-        public Project findProjectByIdentifier(String projectId){
+    }
+
+
+
+        public Project findProjectByIdentifier(String projectId, String username){
+
             //Only want to return the project if the user looking for it is the owner
+
             Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
             if(project == null){
                 throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
+
             }
+
+            if(!project.getProjectLeader().equals(username)){
+                throw new ProjectNotFoundException("Project not found in your account");
+            }
+
+
+
             return project;
         }
-        public Iterable<Project> findAllProjects(){
-            return projectRepository.findAll();
-        }
-        public void deleteProjectByIdentifier(String projectid){
-            Project project = projectRepository.findByProjectIdentifier(projectid.toUpperCase());
-            if(project == null){
-                throw  new  ProjectIdException("Cannot Project with ID '"+projectid+"'. This project does not exist");
+
+
+            public Iterable<Project> findAllProjects(String username){
+                return projectRepository.findAllByProjectLeader(username);
             }
-            projectRepository.delete(project);
-        }
-    }
+
+
+                public void deleteProjectByIdentifier(String projectid, String username){
+
+
+
+
+                    projectRepository.delete(findProjectByIdentifier(projectid, username));
+                }
+
+            }
--- PPMToolJava/src/main/java/com/hcl/web/ProjectController.java
+++ PPMToolJava/src/main/java/com/hcl/web/ProjectController.java
@@ -21,30 +21,36 @@ public class ProjectController {
     private ProjectService projectService;
     @Autowired
     private MapValidationErrorService mapValidationErrorService;
-
-
     @PostMapping("")
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result, Principal principal){
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap!=null) return errorMap;
+        Project project1 = projectService.saveOrUpdateProject(project, principal.getName());
+        return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
+    }
 
-        public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result, Principal principal){
 
-            ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
-            if(errorMap!=null) return errorMap;
+    @GetMapping("/{projectId}")
 
-           
-            Project project1 = projectService.saveOrUpdateProject(project, principal.getName());
-            return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
-        }
+        public ResponseEntity<?> getProjectById(@PathVariable String projectId, Principal principal){
 
-        @GetMapping("/{projectId}")
-        public ResponseEntity<?> getProjectById(@PathVariable String projectId){
-            Project project = projectService.findProjectByIdentifier(projectId);
+
+            Project project = projectService.findProjectByIdentifier(projectId, principal.getName());
+
             return new ResponseEntity<Project>(project, HttpStatus.OK);
         }
+
+
         @GetMapping("/all")
-        public Iterable<Project> getAllProjects(){return projectService.findAllProjects();}
+
+        public Iterable<Project> getAllProjects(Principal principal){return projectService.findAllProjects(principal.getName());}
+
+
         @DeleteMapping("/{projectId}")
-        public ResponseEntity<?> deleteProject(@PathVariable String projectId){
-            projectService.deleteProjectByIdentifier(projectId);
-            return new ResponseEntity<String>("Project with ID: '"+projectId+"' was deleted", HttpStatus.OK);
+       
+            public ResponseEntity<?> deleteProject(@PathVariable String projectId, Principal principal){
+                projectService.deleteProjectByIdentifier(projectId, principal.getName());
+
+                return new ResponseEntity<String>("Project with ID: '"+projectId+"' was deleted", HttpStatus.OK);
+            }
         }
-    }
