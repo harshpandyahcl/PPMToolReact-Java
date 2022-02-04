c24b684cf53ad882cfc8dd48511642aead516d77
harshpandyahcl <harsh.pandya@hcl.com>
2022-02-03 20:38:59 -0600

    sec7 done

--- ppmtoolreactjs/src/components/ProjectBoard/ProjectBoard.js
+++ ppmtoolreactjs/src/components/ProjectBoard/ProjectBoard.js
@@ -4,7 +4,6 @@ import Backlog from "./Backlog";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { getBacklog } from "../../actions/backlogActions";
-
 class ProjectBoard extends Component {
   //constructor to handle errors
   constructor() {
@@ -13,31 +12,34 @@ class ProjectBoard extends Component {
       errors: {}
     };
   }
-
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.getBacklog(id);
   }
-
   componentWillReceiveProps(nextProps) {
     if (nextProps.errors) {
       this.setState({ errors: nextProps.errors });
     }
   }
-
   render() {
     const { id } = this.props.match.params;
     const { project_tasks } = this.props.backlog;
     const { errors } = this.state;
-
     let BoardContent;
 
     const boardAlgorithm = (errors, project_tasks) => {
       if (project_tasks.length < 1) {
+        //PROJECT IDENTIFIER BUG
         if (errors.projectNotFound) {
           return (
             <div className="alert alert-danger text-center" role="alert">
               {errors.projectNotFound}
+            </div>
+          );
+        } else if (errors.projectIdentifier) {
+          return (
+            <div className="alert alert-danger text-center" role="alert">
+              {errors.projectIdentifier}
             </div>
           );
         } else {
@@ -51,9 +53,7 @@ class ProjectBoard extends Component {
         return <Backlog project_tasks_prop={project_tasks} />;
       }
     };
-
     BoardContent = boardAlgorithm(errors, project_tasks);
-
     return (
       <div className="container">
         <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
@@ -61,26 +61,20 @@ class ProjectBoard extends Component {
         </Link>
         <br />
         <hr />
-        
         {BoardContent}
       </div>
     );
   }
 }
-
 ProjectBoard.propTypes = {
   backlog: PropTypes.object.isRequired,
-
   getBacklog: PropTypes.func.isRequired,
   errors: PropTypes.object.isRequired
 };
-
 const mapStateToProps = state => ({
- 
   backlog: state.backlog,
   errors: state.errors
 });
-
 export default connect(
   mapStateToProps,
   { getBacklog }
--- ppmtoolreactjs/src/store.js
+++ ppmtoolreactjs/src/store.js
@@ -1,20 +1,23 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-
 const initalState = {};
 const middleware = [thunk];
 
 let store;
 
-if (window.navigator.userAgent.includes("Chrome")) {
+
+const ReactReduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+if (window.navigator.userAgent.includes("Chrome") && ReactReduxDevTools) {
   store = createStore(
     rootReducer,
     initalState,
     compose(
       applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+    
+      ReactReduxDevTools
     )
   );
 } else {
@@ -24,5 +27,4 @@ if (window.navigator.userAgent.includes("Chrome")) {
     compose(applyMiddleware(...middleware))
   );
 }
-
 export default store;
